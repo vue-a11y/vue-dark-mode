84417d5ff281e68978e49c311332918f48702eff
Alan Ktquez <ktquez@gmail.com>
2020-02-29 09:52:53 -0300

    Add style into component

--- src/vue-dark-mode.vue
+++ src/vue-dark-mode.vue
@@ -15,10 +15,32 @@
 </template>
 
 <script>
-import { styles } from './styles'
+const styles = `
+  html {
+    background-color: #222 !important;
+    color: #333 !important;
+  }
+
+  body {
+    filter: contrast(90%) invert(90%) hue-rotate(180deg) !important;
+    -ms-filter: invert(100%);
+    -webkit-filter: contrast(90%) invert(90%) hue-rotate(180deg) !important;
+  }
+
+  input, textarea, select {
+    color: purple;
+  }
+
+  img, video, iframe, canvas, svg, embed[type='application/x-shockwave-flash'], object[type='application/x-shockwave-flash'], *[style*='url('] {
+    filter: invert(100%) hue-rotate(-180deg) !important;
+    -ms-filter: invert(100%) !important;
+    -webkit-filter: invert(100%) hue-rotate(-180deg) !important;
+  }
+`
 
 export default {
   name: 'DarkMode',
+
   props: {
     isDark: {
       type: Boolean,
@@ -49,6 +71,7 @@ export default {
       default: '#999'
     }
   },
+
   data () {
     return {
       darkMode: false,
@@ -56,6 +79,7 @@ export default {
       $_use_filter: false
     }
   },
+
   created () {
     this.darkMode = this.isDark
     if (!this.$isServer) {
@@ -67,6 +91,7 @@ export default {
       if (this.darkMode) this.setDarkMode()
     }
   },
+
   methods: {
     supportsFilters () {
       const div = document.createElement('div')
@@ -74,25 +99,31 @@ export default {
       if (!isSupported) console.warn('CSS filter is not supported')
       return isSupported
     },
+
     prefersDark () {
       return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
     },
+
     setThemeColor (color) {
       if (this.themeColorMeta) return this.themeColorMeta.setAttribute('content', color)
     },
+
     toggleClass () {
       document.documentElement.classList.toggle(this.className)
     },
+
     setDarkMode () {
       window[this.persist].setItem('darkMode', 'on')
       this.toggleClass()
       this.setThemeColor(this.themeColorDark)
     },
+
     removeDarkMode () {
       window[this.persist].removeItem('darkMode')
       this.toggleClass()
       this.setThemeColor(this.themeColorLight)
     },
+
     toggleDarkMode () {
       this.darkMode = !this.darkMode
       if (this.darkMode) return this.setDarkMode()
@@ -103,6 +134,13 @@ export default {
 </script>
 
 <style>
+body {
+  text-rendering: optimizeSpeed;
+  image-rendering: optimizeSpeed;
+  -webkit-font-smoothing: antialiased;
+  -webkit-image-rendering: optimizeSpeed;
+}
+
 .vue-dark-mode {
   appearance: none;
   background-color: transparent;
